58856a7349955d73ec8a55eac874ad3936002980
Natechawin <natechawin@gmail.com>
2021-01-28 23:24:51 +0700

    Reformat code

--- src/test/scala/dojo/leetcode/easy/most_common_word/solutions/SolutionTest.scala
+++ src/test/scala/dojo/leetcode/easy/most_common_word/solutions/SolutionTest.scala
@@ -5,29 +5,29 @@ import org.scalatest.wordspec.AnyWordSpec
 import matchers.should.Matchers._
 
 class SolutionTest extends AnyWordSpec {
-    "mostCommonWord" when {
-        "Given word without given banned word" should {
+  "mostCommonWord" when {
+    "Given word without given banned word" should {
 
-            val givenWord = "Bob hit a ball, the hit ball flew far after it was hit."
-            val bannedWord = Array[String]()
+      val givenWord = "Bob hit a ball, the hit ball flew far after it was hit."
+      val bannedWord = Array[String]()
 
-            "Return most frequent word" in {
-                val result = Solution.mostCommonWord(givenWord, bannedWord)
+      "Return most frequent word" in {
+        val result = Solution.mostCommonWord(givenWord, bannedWord)
 
-                result shouldEqual "hit"
-            }
-        }
-
-        "Given word with banned word" should {
-
-            val givenWord = "Bob hit a ball, the hit ball flew far after it was hit."
-            val bannedWord = Array[String]{"hit"}
-
-            "Return most frequent word but not the banned one" in {
-                val result = Solution.mostCommonWord(givenWord, bannedWord)
-
-                result shouldEqual "ball"
-            }
-        }
+        result shouldEqual "hit"
+      }
     }
+
+    "Given word with banned word" should {
+
+      val givenWord = "Bob hit a ball, the hit ball flew far after it was hit."
+      val bannedWord = Array[String] { "hit" }
+
+      "Return most frequent word but not the banned one" in {
+        val result = Solution.mostCommonWord(givenWord, bannedWord)
+
+        result shouldEqual "ball"
+      }
+    }
+  }
 }
