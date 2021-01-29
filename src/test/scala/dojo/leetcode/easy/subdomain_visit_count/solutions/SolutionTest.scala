f090d2a5f5927b0411c52c7b0559208a64436349
Natechawin <natechawin@gmail.com>
2021-01-30 04:18:36 +0700

    Add more test case for subDomainVisitCount

--- src/test/scala/dojo/leetcode/easy/subdomain_visit_count/solutions/SolutionTest.scala
+++ src/test/scala/dojo/leetcode/easy/subdomain_visit_count/solutions/SolutionTest.scala
@@ -5,20 +5,46 @@ import org.scalatest.wordspec.AnyWordSpec
 import matchers.should.Matchers._
 
 class SolutionTest extends AnyWordSpec {
+
   "subdomainVisits" when {
     "Given array of cpDomain which contain number of visit and domain" should {
 
-      val cpDomains = Array[String] { "9001 discuss.leetcode.com" }
+      val testCases = Seq(
+        SubdomainVisitCountTestCase(
+          cpDomainInput = Array("9001 discuss.leetcode.com"),
+          expectedResult =
+            Array("9001 discuss.leetcode.com", "9001 leetcode.com", "9001 com")
+        ),
+        SubdomainVisitCountTestCase(
+          cpDomainInput = Array(
+            "900 google.mail.com",
+            "50 yahoo.com",
+            "1 intel.mail.com",
+            "5 wiki.org"
+          ),
+          expectedResult = Array(
+            "901 mail.com",
+            "50 yahoo.com",
+            "900 google.mail.com",
+            "5 wiki.org",
+            "5 org",
+            "1 intel.mail.com",
+            "951 com"
+          )
+        )
+      )
 
       "Return most frequent word" in {
-        val result = Solution.subdomainVisits(cpDomains)
 
-        result.length shouldEqual 3
+        for (testCase <- testCases) {
+          val subdomainCounts = Solution.subdomainVisits(testCase.cpDomainInput)
 
-        result.contains("9001 discuss.leetcode.com") shouldBe true
-        result.contains("9001 leetcode.com") shouldBe true
-        result.contains("9001 com") shouldBe true
+          subdomainCounts.length should equal(testCase.expectedResult.length)
 
+          for (subdomainCount <- subdomainCounts) {
+            testCase.expectedResult.contains(subdomainCount) should be(true)
+          }
+        }
       }
     }
   }
--- src/test/scala/dojo/leetcode/easy/subdomain_visit_count/solutions/TestCase.scala
+++ src/test/scala/dojo/leetcode/easy/subdomain_visit_count/solutions/TestCase.scala
@@ -1,2 +1,6 @@
 package dojo.leetcode.easy.subdomain_visit_count.solutions
 
+case class SubdomainVisitCountTestCase(
+    cpDomainInput: Array[String],
+    expectedResult: Array[String]
+)
